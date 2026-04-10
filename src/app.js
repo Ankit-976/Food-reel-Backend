8012725826a63d402f67845d06d6c7d67a314e66
Ankit-976 <ankitbasa113@gmail.com>
2026-04-10 20:55:56 +0530

    fix cors for deployment

--- src/app.js
+++ src/app.js
@@ -9,12 +9,15 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true
 }))
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/food', foodRoutes);
 app.use('/api/food-partner', foodPartnerRoutes);
